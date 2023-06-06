89e4d851275ce75b828260a7ff164e64eed88aef
JBattishill <jbattishill91@gmail.com>
2023-06-06 11:17:14 +1000

    Jarryd Init Push

---
+++ app/profiles/page.tsx
@@ -0,0 +1,13 @@
+'use client';
+
+
+import Player from "./Player"
+
+export default function MapPage() {
+  return (
+    <main className="items-center bgDarkGrey">
+
+      <Player/>
+    </main>
+  )
+}
