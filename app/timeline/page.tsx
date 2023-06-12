2393e45edfca57ccd8f216f310dadc3e89a4d758
JBattishill <jbattishill91@gmail.com>
2023-06-12 13:57:34 +1000

    pushing before trying to set React version to 17 for TimelineJS

--- app/timeline/page.tsx
+++ app/timeline/page.tsx
@@ -1,14 +1,16 @@
 'use client';
-import { Timeline } from '@knight-lab/timelinejs';
-import '@knight-lab/timelinejs/dist/css/timeline.css';
+// import { Timeline } from '@knight-lab/timelinejs';
+// import '@knight-lab/timelinejs/dist/css/timeline.css';
+import TimelineComponent from './TimelineComponent';
 
-export default function MapPage() {
+export default function TimelinePage() {
 
  
 
   return (
     <main className="items-center bgDarkGrey">
       <h2 className='title text-size-2x h-screen w-screen'>We're Still working on this...</h2>
+      <TimelineComponent />
     </main>
   )
 }
