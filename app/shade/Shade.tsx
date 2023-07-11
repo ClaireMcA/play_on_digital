a4c4d03daaaf59fe22d4e6ea45248bda34dc84a7
JBattishill <jbattishill91@gmail.com>
2023-07-11 14:26:30 +1000

    Update Shade.tsx

--- app/shade/Shade.tsx
+++ app/shade/Shade.tsx
@@ -3,7 +3,7 @@ export default function Shade(){
     return (
      
      <div className="flex text-center items-center h-screen w-screen mx-auto">
-        <video autoPlay className="z-0 place-self-center h-auto w-screen" controls loop>
+        <video autoPlay className="z-0 place-self-center h-auto w-screen" controls loop muted>
                 <source
                 src="/videos/testVid.mp4"
                 type="video/mp4"
